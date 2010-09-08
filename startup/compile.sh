0dbee9ddd3218b03f9f8fbc56f5c1111c146d41f
joey <joey@hwi.ath.cx>
2010-09-08 18:27:08 +0100

    normal shift-boot

--- startup/compile.sh
+++ startup/compile.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 # cl65 -Oirs --cpu 65C02 -t bbc --listing file.c -o prog
-pasta -a demo.s -o !boot
+pasta -a demo.s -o go
 # adfs disk.adl .
-cp '!boot' '!boot.inf' "$OUTPUTDISK"
+cp '!boot' '!boot.inf' 'go' 'go.inf' "$OUTPUTDISK"
 
