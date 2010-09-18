92273aefc00290b4e267269aa6c38c33c5de7918
joey <joey@hwi.ath.cx>
2010-09-18 15:59:47 +0100

    need ^M in 'git add 'startup/!boot' joeys/grab_file.sh'boot input file

--- joeys/grab_file.sh
+++ joeys/grab_file.sh
@@ -15,14 +15,14 @@ then
 fi
 
 if [ "$*" = "" ]
-then ./bbcim/bbcim -c disk.ssd ; exit
+then bbcim -c disk.ssd ; exit
 fi
 
 cd mycode.conv || exit 9
 
 for FILE in "$@"
 do
-  ../bbcim/bbcim -es ../disk.ssd "$FILE"
+  bbcim -es ../disk.ssd "$FILE"
   # cat "$FILE" | sh ../fromunix.sh -tounix | dog "$FILE"
 done
 
